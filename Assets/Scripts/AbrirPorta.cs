efa5728838af6202431e64b8473e64fa20916bdc
Kaio de Souza Gomes <56899434+NeoFullBurst@users.noreply.github.com>
2021-09-24 21:05:35 -0300

    HUD da porta + Comandos
    HUD da porta;
    Animação da porta;
    Programação.

---
+++ Assets/Scripts/AbrirPorta.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbrirPorta : MonoBehaviour {
+	public float distancia;
+	public GameObject acaoDisplay; // hud tecla de ação.
+	public GameObject acaoTxt; // hud da exibição da ação.
+	public GameObject porta; // gameObject da porta.
+	public AudioSource rangerSom; // som de abertura da porta.
+
+	// Update is called once per frame
+	void Update () {
+		distancia = PlayerCasting.distanciaAlvo; // pegando o valor da variável do RayCast anterior.
+
+	}
+	void OnMouseOver () { // quando o mouse está sobre o objeto.
+		if (distancia <= 2) {
+			acaoDisplay.SetActive (true);
+			acaoTxt.SetActive (true);
+		}
+		if (Input.GetButtonDown ("BtnAcao")){ // quando apertar o botão.
+			if (distancia <= 2) { // e a distância for menor ou igual a 2.
+				this.GetComponentInChildren<BoxCollider>().enabled = false; // desliga o collider da porta.
+				acaoDisplay.SetActive (false); 
+				acaoTxt.SetActive (false);
+				porta.GetComponent<Animation>().Play("porta01anim"); // iniciando a animação da porta.
+				rangerSom.Play(); // iniciando o som.
+			}
+			
+		}
+		if (distancia >= 2) {
+			acaoDisplay.SetActive (false);
+			acaoTxt.SetActive (false);
+		}
+	}
+	void OnMouseExit () { // quando o mouse não está a frente do objeto.
+		acaoDisplay.SetActive (false);
+		acaoTxt.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour {
+
+	public static float distanciaAlvo;// variável estática de distância ao alvo
+	public float alvo; // variável até o alvo
+	// Update is called once per frame
+	void Update () {
+		RaycastHit Hit; // variável de RayCast
+		// verificando se o RayCast atingiu algum Collider
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)){
+			alvo = Hit.distance;
+			distanciaAlvo = alvo;
+		}
+	}
+}
